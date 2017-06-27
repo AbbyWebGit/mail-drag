be8de3ead68b305a9ace9cd7e71b6e8e378cb242
AbbyWebGit <abby_zj@foxmail.com>
2017-06-27 17:37:04 +0800

    no message

--- mail-drag/js/index.js
+++ mail-drag/js/index.js
@@ -26,32 +26,34 @@ var list = [{
 }]
 
 
- // <li>
- //                <input  class="select-btn" type="checkbox">
- //                <div class="mail-content">
- //                    <span>秒味课堂1</span>
- //                    <span class="right">2015-12-30</span>
- //                </div>
- //                <p>JS拖拽的进一步学习,移动与拉伸JS拖拽的进一步练习</p>
- //            </li>
+
 
 $(function() {
 
     function dataIndex() {
         var len = list.length;
         console.log(len)
-        var liHtml="";
-        for(i=0;i<len;i++){
-            liHtml+='<li data-id="'+list[i].id+'"><input class="select-btn" type="checkbox"><div class="mail-content"><span>'
-            +list[i].caption+'</span><span class="right">'+list[i].time+'</span></div><p>'+list[i].desc+'</p></li>'
+        var liHtml = "";
+        for (i = 0; i < len; i++) {
+            liHtml += '<li data-id="' + list[i].id + '"><input class="select-btn" type="checkbox"><div class="mail-content"><span>' + list[i].caption + '</span><span class="right">' + list[i].time + '</span></div><p>' + list[i].desc + '</p></li>'
 
         }
         $("#dataList").html(liHtml);
 
-
-
-
     }
+
     dataIndex();
+    $("#all-select").on('click', function() {
+        var len = $("#dataList li").length;
+        var aa=$(this).attr("checked")==true;
+        console.log(aa)
+        if ($(this).attr("checked") == false) {
+            console.log(11)
+            for (i = 0; i < len; i++) {
+                $("#dataList li").eq(i).find("input").attr("checked", "checked");
+            }
+        }
+
+    });
 
 })
